f7d03e7fc139ee2243c64a198c289354bc998ea2
MarkBezsonov <markbezsonov@gmail.com>
2021-02-02 18:30:22 -0600

    commit tree 904eb4b9f96c5d224f9cbcf371a9d31b0d780d7e

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(greeting)
+    if greeting == "I love you grandma!".upcase
+        "I love you too pumpkin!".upcase
+    elsif greeting == greeting.upcase
+        return "NO, NOT SINCE 1938!"
+    else
+        return "HUH?! SPEAK UP, SONNY!"
+    end
+end
